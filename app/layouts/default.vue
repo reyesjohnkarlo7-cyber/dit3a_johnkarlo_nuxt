41aa34fd56df02ef08ad26c5ba0c2cbae6016ab4
STUDENT 22 <reyesjohnkarlo7@gmail.com>
2025-11-05 03:20:54 +0000

    first commit

---
+++ app/layouts/default.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <v-app>
+
+      <v-navigation-drawer
+        v-model="drawer"
+      >
+        <v-list>
+          <v-list-item
+            prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
+            subtitle="reyesjohnkarlo7@gmail.com"
+            title="John Karlo Reyes"
+          ></v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
+          <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-app-bar :elevation="2">
+  <template v-slot:prepend>
+    <v-app-bar-nav-icon @click.stop="drawer =! drawer"></v-app-bar-nav-icon>
+
+  </template>
+
+  <v-app-bar-title>Reyes John Karlo - DIT3A</v-app-bar-title>
+</v-app-bar>
+
+  <v-main>
+    <v-container fluid>
+      <slot/>
+    </v-container>
+  </v-main>
+  </v-app>
+
+  </div>
+</template>
+
+<script setup>
+  const drawer = ref(true)
+</script>
---
+++ app/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Hello Sir Raiven, Getwell po</h1>
+</template>
